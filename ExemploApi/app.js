a75da4a6bcb1a38d40d47e0ae0a555319730258f
Gesley Rosa <geeh.oliveira@hotmail.com>
2024-03-26 22:02:40 -0300

    att

---
+++ ExemploApi/app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+let names = []
+
+app.get('/', (req, res) => {
+  res.json(names)
+})
+
+/*app.post('/', (req, res) => {
+    names.push(req.body.name)
+    res.send(`Lendo nome: ${req.body.name}`)
+  })*/
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+function calculaTemp(type, temperature){
+    switch(type){
+        case "C":
+            let farenheit = ((9*temperature)/5) + 32;
+            return `Conversão: F | Temperatura: ${farenheit}`
+        case "F":
+            let celsius = (5*(temperature-32))/9;
+            return `Conversão: C | Temperatura: ${celsius}`
+    }
+} 
+
+/*app.post('/', (req, res) => {
+    res.send(`Tipo: ${req.body.type}
+              Temperatura: ${req.body.temperature}
+              ${calculaTemp(req.body.type, req.body.temperature)}`
+             )
+    
+  })*/
+
+
+app.post('/', (req, res) => {
+    let qtd = req.query.qtd;
+    let notas = req.body.nota;
+    res.send(`Quantidade: ${qtd} | notas: ${notas}`)
+  })
+
+/*app.post('/media', (req, res) => {
+    names.push(req.body.name)
+    res.send(`Lendo nome: ${req.body.name}`)
+  })*/
