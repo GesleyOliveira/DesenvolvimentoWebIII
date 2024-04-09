bd19b974ab5a658901830eff02dbd903ac273dc5
Gesley Rosa <geeh.oliveira@hotmail.com>
2024-04-09 19:41:44 -0300

    att

--- ExemploApi/app.js
+++ ExemploApi/app.js
@@ -1,53 +1,29 @@
-const express = require('express')
-const app = express()
 const port = 3000
+
+let items = []
+const veiculos = [{id: 1, name: "Fiat"}, {id: 2, name: "Celta"}, {id: 3, name: "Marea"}];
 
 app.use(express.json())
 
-let names = []
+app.post('/', (req, res) => {
+    items.push(req.body.name)
+    res.send(`Post ${req.body.name}`)
+  })
 
-app.get('/', (req, res) => {
-  res.json(names)
-})
+app.put('/', (req, res) => {
+    const index = veiculos.findIndex(x => x.id == req.query.id);
+    veiculos[index] = {id: req.query.id, name: req.body.name}
+    res.send(JSON.stringify(veiculos))
+  })
 
-/*app.post('/', (req, res) => {
-    names.push(req.body.name)
-    res.send(`Lendo nome: ${req.body.name}`)
-  })*/
+  app.delete('/', (req, res) => {
+    const index = veiculos.findIndex(x => x.id == req.query.id);
+    veiculos.splice(index, 1);
+    res.send(JSON.stringify(veiculos))
+  })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-function calculaTemp(type, temperature){
-    switch(type){
-        case "C":
-            let farenheit = ((9*temperature)/5) + 32;
-            return `Conversão: F | Temperatura: ${farenheit}`
-        case "F":
-            let celsius = (5*(temperature-32))/9;
-            return `Conversão: C | Temperatura: ${celsius}`
-    }
-} 
-
-/*app.post('/', (req, res) => {
-    res.send(`Tipo: ${req.body.type}
-              Temperatura: ${req.body.temperature}
-              ${calculaTemp(req.body.type, req.body.temperature)}`
-             )
-    
-  })*/
-
-
-app.post('/', (req, res) => {
-    let qtd = req.query.qtd;
-    let notas = req.body.nota;
-     abaixoDaMedia = notas.filter( => (nota < 60));
-    res.send(`Quantidade: ${qtd} | notas: ${notas}
-            ${abaixoDaMedia}`)
-  })
-
-/*app.post('/media', (req, res) => {
-    names.push(req.body.name)
-    res.send(`Lendo nome: ${req.body.name}`)
-  })*/
